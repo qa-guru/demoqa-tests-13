f86ce3e6750e85e53d4aa8da0779a7e4834c8b07
svasenkov <aanher@gmail.com>
2022-05-26 22:21:02 +0300

    added selenide textbox tests

--- src/test/java/tests/JUnit5Examples.java
+++ src/test/java/tests/JUnit5Examples.java
@@ -2,6 +2,8 @@ package tests;
 
 import org.junit.jupiter.api.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class JUnit5Examples {
 
     @BeforeAll
@@ -27,6 +29,7 @@ public class JUnit5Examples {
     @Test
     void secondTest() {
         System.out.println("========= Started secondTest");
+        assertEquals(2, 3);
     }
 
     @AfterEach
