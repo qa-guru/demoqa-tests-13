ed046527a420c3a67e805991441212bf543d18c2
svasenkov <aanher@gmail.com>
2022-05-26 21:02:52 +0300

    added junit5 examples

---
+++ src/test/java/tests/JUnit5Examples.java
@@ -0,0 +1,41 @@
+package tests;
+
+import org.junit.jupiter.api.*;
+
+public class JUnit5Examples {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("== Something before All tests");
+    }
+
+    @BeforeAll
+    static void beforeAll1() {
+        System.out.println("== Something 1 before All tests");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("===== Something before Every tests");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("========= Started firstTest");
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("========= Started secondTest");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("===== Something after Every tests");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("== Something after All tests");
+    }
+}
